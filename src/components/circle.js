b376b38d72c4b460c798eb37ed291d69db4a3b40
Ubaid Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-11-30 20:39:15 +0530

    shape

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,15 @@
 
 // declare class
 
-
 // export class using module.exports
+
+import Shape from "./shape";
+// declare class
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.color = "red";
+  }
+  calculateArea() {}
+}
+export default Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
 // declare class
 
 // export class using module.exports
+
+class Shape{
+  constructor()
+  {
+    this.color="blue";
+  }
+  drawShape(){}
+  calculateArea(){}
+};
+export default Shape;
